da30efabf6a022fa064dcae241644b04b61e0fb4
catroll <ninedoors@126.com>
2018-05-23 12:17:12 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+#
+# Copyright 2009 SendCloud
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from setuptools import setup
+
+version = '0.0.1'
+
+with open('requirements.txt') as _file:
+    requires = [i.strip() for i in _file]
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name='lian',
+    version=version,
+    packages=['lian'],
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    author='Hu Ang',
+    author_email='ninedoors@126.com',
+    url='https://www.sendcloud.com/',
+    description='lian is a python toolkit',
+    long_description=long_description,
+    install_requires=requires,
+    python_requires='>=3.4',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
