7f0763b019d05d9dfd9d05dde4f49a02ae5804b3
catroll <ninedoors@126.com>
2018-06-05 17:34:34 +0800

    Update lian/utils/words.py

--- lian/utils/words.py
+++ lian/utils/words.py
@@ -6,17 +6,17 @@ def int2ordinal(num):
     return '%d%s' % (num, {1: 'st', 2: 'nd', 3: 'rd'}.get(num % 10, 'th'))
 
 
-def _test_int2ordinal():
+def __test_int2ordinal():
     for i in range(1, 100):
         print(int2ordinal(i))
 
 
-def _test():
+def __test():
     for name, method in globals().items():
-        if name.startswith('_test_') and callable(method):
+        if name.startswith('__test_') and callable(method):
             print('\n' + (' RUN: %s ' % name).center(100, '-') + '\n')
             method()
 
 
 if __name__ == '__main__':
-    _test()
+    __test()
