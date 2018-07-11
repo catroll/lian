818b83c92fc0cc9aeb5b115ea1a77a0de002166d
catroll <ninedoors@126.com>
2018-07-11 10:32:00 +0800

    Update lian/dispatcher.py

--- lian/dispatcher.py
+++ lian/dispatcher.py
@@ -3,6 +3,44 @@
 """
 https://github.com/django/django/blob/2.0.7/django/dispatch/dispatcher.py
 modified by Hu Ang <ninedoors@126.com>
+
+License: https://raw.githubusercontent.com/django/django/2.0.7/django/dispatch/license.txt
+
+django.dispatch was originally forked from PyDispatcher.
+
+PyDispatcher License:
+
+    Copyright (c) 2001-2003, Patrick K. O'Brien and Contributors
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+        Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+
+        Redistributions in binary form must reproduce the above
+        copyright notice, this list of conditions and the following
+        disclaimer in the documentation and/or other materials
+        provided with the distribution.
+
+        The name of Patrick K. O'Brien, or the name of any Contributor,
+        may not be used to endorse or promote products derived from this
+        software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+    INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+    HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+    STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+    OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import threading
@@ -293,3 +331,38 @@ def receiver(signal, **kwargs):
         return func
 
     return _decorator
+
+
+def __test():
+    import logging
+
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s')
+
+    pizza_done = Signal(providing_args=["toppings", "size"])
+
+    class PizzaStore:
+        def send_pizza(self, toppings, size):
+            logging.info('%s.send_pizza: toppings %r, size %r', self.__class__.__name__, toppings, size)
+            pizza_done.send(sender=self.__class__, toppings=toppings, size=size)
+
+    class PizzaStoreA(PizzaStore):
+        pass
+
+    class PizzaStoreB(PizzaStore):
+        pass
+
+    @receiver(pizza_done, sender=PizzaStoreA, dispatch_uid='pizza_store_a.pizza_done')
+    def what_we_do_when_pizza_done_a(*args, **kwargs):
+        logging.info('PizzaStoreA.pizza_done: args %r, kwargs %r', args, kwargs)
+
+    @receiver(pizza_done, sender=PizzaStoreB, dispatch_uid='pizza_store_b.pizza_done')
+    def what_we_do_when_pizza_done_b(signal, sender, toppings, size):
+        logging.info('PizzaStoreB.pizza_done: signal %r, sender %r', signal, sender)
+        logging.info('PizzaStoreB.pizza_done: toppings %r, size %r', toppings, size)
+
+    PizzaStoreA().send_pizza('seafood', 12)
+    PizzaStoreB().send_pizza('seafood', 12)
+
+
+if __name__ == '__main__':
+    __test()
