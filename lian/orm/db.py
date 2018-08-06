ffef6bd3c8d76781780b035620cf68bc76d2bcb0
catroll <ninedoors@126.com>
2018-08-06 18:34:12 +0800

    Update lian/orm/db.py

--- lian/orm/db.py
+++ lian/orm/db.py
@@ -2,6 +2,19 @@
 
 """
 DB Connection
+
+host=None, user=None, password="",
+database=None, port=0, unix_socket=None,
+charset='', sql_mode=None,
+read_default_file=None, conv=None, use_unicode=None,
+client_flag=0, cursorclass=Cursor, init_command=None,
+connect_timeout=10, ssl=None, read_default_group=None,
+compress=None, named_pipe=None,
+autocommit=False, db=None, passwd=None, local_infile=False,
+max_allowed_packet=16*1024*1024, defer_connect=False,
+auth_plugin_map=None, read_timeout=None, write_timeout=None,
+bind_address=None, binary_prefix=False, program_name=None,
+server_public_key=None
 """
 
 from __future__ import absolute_import, print_function
