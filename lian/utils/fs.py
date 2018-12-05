563fbcef7567747cd2e155c0553bbf6678f08dc9
catroll <ninedoors@126.com>
2018-12-05 17:54:45 +0800

    Update lian/utils/fs.py

--- lian/utils/fs.py
+++ lian/utils/fs.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import errno
 import os
 
 import six
@@ -9,8 +10,12 @@ def mkdir(path, mode=0o777, exist_ok=False):
     if six.PY2:
         try:
             os.makedirs(path, mode)
+        # Python >2.5 (except OSError, error: for Python <2.5)
         except OSError as error:
-            if not exist_ok or 'File exists' not in str(error):
-                raise
+            # if 'File exists' in str(error):
+            if error.errno == errno.EEXIST and os.path.isdir(path):
+                if exist_ok:
+                    return
+            raise
     else:
         os.makedirs(path, mode, exist_ok)
